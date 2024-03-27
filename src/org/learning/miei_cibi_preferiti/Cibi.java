a483753a5ea08e7fed2c642c6936bc60f7a90ffe
Francesco Carrara <f.carrara24@studenti.unibg.it>
2024-03-27 15:42:42 +0100

    fixed average food

--- src/org/learning/miei_cibi_preferiti/Cibi.java
+++ src/org/learning/miei_cibi_preferiti/Cibi.java
@@ -5,7 +5,7 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 public class Cibi {
     public static void main(String[] args) {
         String[] cibi = {"pizza", "kebap", "sushi", "paccheri ai gamberi", "spaghetti al sugo"};
-        int media =(int) Math.floor(cibi.length/ 2.0 );
+        int media = cibi.length/ 2;
 
         //cibo preferito
         System.out.println("cibo preferito:      "+cibi[0]);
